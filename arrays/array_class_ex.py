fd2a08d0cee8faef3e9185921134c72d8f8c562d
Tom Fyfe <tomfyfe85@gmail.com>
2024-06-27 09:04:07 +0100

    used dict comprehension in removeAt

--- arrays/array_class_ex.py
+++ arrays/array_class_ex.py
@@ -14,8 +14,9 @@ class ArrayClass:
         self.lst.append(item)
         
     def removeAt(self, index):
-        del self.lst[index]
-        
+        self.lst = [item for i, item in enumerate(self.lst) if i != index]
+        # del self.lst[index]
+
     def indexOf(self, item_to_find):
         return self.lst.index(item_to_find)
         
@@ -26,12 +27,8 @@ array.insert(10)
 array.insert(20)
 array.insert(30)
 
+array.removeAt(1)
 for item in array.lst:
         print(item)
-
-# array.removeAt(1)
-print("\n")
-# for item in array.lst:
-#         print(item)
         
-print(array.indexOf(20))
+        
